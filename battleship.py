69018775ddea468b036e6c681a91d8ae14f6cc48
Christina Foran <cforan99@gmail.com>
2016-01-04 12:57:23 -0800

    wrote an initialize_board function to create an nXn board with letter and number row and column labels

--- battleship.py
+++ battleship.py
@@ -1,9 +1,28 @@
+#TO DO: Add turn count and end the game if turn limit is reached or if position is guessed correctly.
+# Make multiple battleships: you'll need to be careful because you need to make sure that you don’t place battleships on top of each other on the game board. You'll also want to make sure that you balance the size of the board with the number of ships so the game is still challenging and fun to play.
+# Make battleships of different sizes: this is trickier than it sounds. All the parts of the battleship need to be vertically or horizontally touching and you’ll need to make sure you don’t accidentally place part of a ship off the side of the board.
+# Make your game a two-player game.
+
 from random import randint
 
 board = []
+ALPHABET = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-for x in range(0, 5):
-    board.append(["O"] * 5)
+def initialize_board(n):
+    """Creates a nxn board with column and row labels."""
+    row = ["/"]
+    for i in range(1, n+1):
+        row.append(str(i))
+    board.append(row)
+    # print " ".join(row)
+    for num in range(1, n+1):
+        row = [ALPHABET[num]]
+        row = row + ["-"] * n
+        board.append(row)
+        # print " ".join(row)
+    return board
+
+board = initialize_board(10)
 
 def print_board(board):
     for row in board:
@@ -11,14 +30,10 @@ def print_board(board):
 
 print_board(board)
 
-def random_row(board):
-    return randint(0, len(board) - 1)
+def random_coordinates(board):
+    return (randint(1, len(board)), randint(1, len(board)))
 
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
+ship_row, ship_col = random_coordinates(board)
 
 print ship_row
 print ship_col
